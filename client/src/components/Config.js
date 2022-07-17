f1c5edbc9c2cfad0b9929667b02289ad3b4bdee1
kyungho <akh1216@gmail.com>
2022-07-17 12:57:05 +0900

    2022_07_17 13:00 5 complate :D

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL ='https://api.themoviedb.org/3/';
+export const API_KEY ='e545fa5dc6955bcb0df7913e243ecae9';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
